2dd737403718aa74e4e83af1b67afb9000fb1702
Junyup Hong <junyup.hong@gmail.com>
2019-04-30 20:19:22 +0900

    pa0

--- course_sched.c
+++ course_sched.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define MAX_LENGTH 4096
 struct CourseEntry {
@@ -8,17 +10,226 @@ struct CourseEntry {
   float difficulty; // 과목의 난이도
 };
 
+void show_struct(struct CourseEntry *ce);
+void set_ce_by_everytime(FILE** fp, struct CourseEntry* ce);
+void set_ce_by_database(FILE** fp, struct CourseEntry *ce);
+void set_topological_sort(struct CourseEntry *ce, struct CourseEntry *sorted_ce, int size);
+void print_sorted_ce(struct CourseEntry *sce, int size);
+
 int main(int argc, char** argv)
 {
-  printf("Hello, World!\n");
+  struct CourseEntry ce[] = {{.difficulty = 5},{.difficulty = 5},{.difficulty = 5},{.difficulty = 5},{.difficulty = 5},{.difficulty = 5}};
+  struct CourseEntry sorted_ce[6];
 
-  printf("You have %d arguments.\n", argc);
-
-  for (int it = 0; it < argc; ++it) {
-	  printf("\n\t%s", argv[it]);
+  // database.csv 가져오기
+  FILE* fp = fopen("./in/database.csv", "r");
+  if (fp == NULL)
+  {
+    fprintf(stderr, "database open error!");
+    return -1;
   }
+  set_ce_by_database(&fp, ce);
+  fclose(fp);
+  
+  // everytime0 가져오기
+  fp = fopen(*(argv+1), "r");
+  //fp = fopen("./in/everytime0.csv", "r");
+  if (fp == NULL)
+  {
+    fprintf(stderr, "everytime open error!");
+    return -1;
+  }
+  set_ce_by_everytime(&fp, ce);
+  fclose(fp);
+  
 
-  printf("\n\nGood job!\n\n");
+  //show_struct(ce);
+  // everytime1 가져오기
+  //fp = fopen("./in/everytime1.csv", "r");
+  //if (fp == NULL)
+  //{
+    //fprintf(stderr, "everytime1 open error!");
+    //return -1;
+  //}
+  //set_ce_by_everytime(&fp, ce);
+  //fclose(fp);
 
+
+  // everytime2 가져오기
+  //fp = fopen("./in/everytime2.csv", "r");
+  //if (fp == NULL)
+  //{
+    //fprintf(stderr, "everytime2 open error!");
+    //return -1;
+  //}
+  //set_ce_by_everytime(&fp, ce);
+  //fclose(fp);
+  
+
+  set_topological_sort(ce, sorted_ce, sizeof(ce)/sizeof(ce[0]));
+  
+  print_sorted_ce(sorted_ce, sizeof(sorted_ce)/sizeof(sorted_ce[0]));
+//  show_struct(sorted_ce);
   return 0;
 }
+
+
+void set_topological_sort(struct CourseEntry *ce, struct CourseEntry *sorted_ce, int size)
+{
+  int count = size;
+  int offset = 0;
+  int delete_index = -1;
+
+  // ce의 길이가 0이 될때까지 실행!
+  while(count > 0)
+  {
+    for(int i = 0; i < count; i++) {
+      struct CourseEntry temp = {
+        .difficulty = 0
+      };
+      for (int j = 0; j < count; j++)
+      {
+        if(ce[j].n_prerequisites != 0) continue;
+        // 난이도가 가장 높은 ce를 뽑아냄
+        if (ce[j].difficulty > temp.difficulty)
+        {
+          temp = ce[j];
+          delete_index = j;
+        } else if(ce[j].difficulty == temp.difficulty)
+        {
+          // 같으면 이름순서!
+          if (strcmp(ce[j].name, temp.name) < 0) {
+            temp = ce[j];
+            delete_index = j;
+          }
+        }
+      }
+
+      // sorted_ce에 넣어주기
+      sorted_ce[offset] = temp;
+      offset++;
+
+      // 삭제
+      // pre에 있는애들 지우기
+      for(int i = 0; i < count; i++)
+      {
+        if(ce[i].n_prerequisites == 1) {
+          if(!strcmp(ce[i].prerequisites[0].name, temp.name)) ce[i].n_prerequisites = 0;
+        } else if (ce[i].n_prerequisites == 2) {
+          if(!strcmp(ce[i].prerequisites[1].name, temp.name)) ce[i].n_prerequisites = 1;
+          else if(!strcmp(ce[i].prerequisites[0].name, temp.name)) {
+            ce[i].n_prerequisites = 1;
+            ce[i].prerequisites[0] = ce[i].prerequisites[1];
+          }
+        }
+      }
+
+      // ce에서 지우기
+      for(int n = delete_index; n < count - 1; n++)
+      {
+        ce[n] = ce[n+1];
+      }
+      count--;
+    }
+  }
+}
+
+void set_ce_by_everytime(FILE** fp, struct CourseEntry* ce)
+{
+  char strBuf[MAX_LENGTH];
+  char *string;
+  int i = 0;
+  
+  while (!feof(*fp))
+  {
+    string = fgets(strBuf, sizeof(strBuf), *fp);
+    if (string == NULL) break;
+    char* str = strtok(string, ",");
+    float num = atof(strtok(NULL, ","));
+
+    for (int i = 0; i < 6; i++)
+    {
+        if(!strcmp(ce[i].name, str))
+        {
+          ce[i].difficulty = num;
+        }
+    }
+    
+    i++; 
+  }
+}
+
+
+void set_ce_by_database(FILE** fp, struct CourseEntry *ce)
+{
+  char strBuf[MAX_LENGTH];
+  char *string;
+  int i = 0;
+
+  while (!feof(*fp))
+  {
+    string = fgets(strBuf, sizeof(strBuf), *fp);
+    if (string == NULL) break;
+  
+   // struct name
+    char* str = strtok(string, ",");
+    strcpy(ce[i].name, str);
+
+    // struct count
+    str = strtok(NULL, ",");
+    int count = atoi(str);
+    ce[i].n_prerequisites = count;
+
+    for(int j = 0; j < count; j++){
+      // 선수과목 이름의 공백, \n 제거
+      str = strtok(NULL, ",");
+
+      if(str[0] == ' ')
+      {
+        for(int l = 1; l < strlen(str); l++)
+        {
+          str[l-1] = str[l];
+        }
+        str[strlen(str)-1] = '\0';
+      }
+      if (str[strlen(str)-1] == '\n') str[strlen(str)-1] = '\0';
+
+
+      // 선수과목들을 임시로 모아둠
+      struct CourseEntry tce[6][2];
+      for(int k = 0; k < i; k++)
+      {
+        if(!strcmp(ce[k].name, str)) tce[i][j] = ce[k];
+      }
+     
+      // 선수과목에 넣어준다
+      ce[i].prerequisites = tce[i];
+    }
+
+    i++;
+  }
+};
+
+void print_sorted_ce(struct CourseEntry* sce, int size)
+{
+  for(int i = 0; i < size; i++)
+  {
+    printf("%s\n", sce[i].name);
+  }
+}
+
+
+void show_struct(struct CourseEntry *ce)
+{
+  for(int i = 0; i < 6; i++) {
+    printf("%d:\n", i);
+    printf("\tname:%s\n", ce[i].name);
+    printf("\tdifficulty:%lf\n", ce[i].difficulty);
+    printf("\tn_prerequisites:%d\n", ce[i].n_prerequisites);
+    for(int j = 0; j<ce[i].n_prerequisites; j++) {
+      printf("\tprerequisites:%s\n", ce[i].prerequisites[j].name);
+    }
+  }
+}
+
+
--- docs/report.md
+++ docs/report.md
@@ -1,16 +1,30 @@
-# Report 
+# PA0:수강계획짜기 보고서 201421023_홍준엽
+## 인적사항
+* 사이버보안학과
+* 201421023
+* 홍준엽
 
-NOTE: You don't have to write the report in English!
+## 과제 수행 정도
+### 과제 수행 완료!
+* 파일 읽어오기 (database.csv, everytime0.csv, everytime1.csv, everytime2.csv,)
+* sorting & 출력
 
-## What I have done
 
-* Blah blah
-* Ulah ulah
 
-## I learned these
+## 과제를 수행하면서 배운 것
+#### C 언어
+* 과거에 사용했던 C언어를 다시 사용해봄으로써 C언어에 대해 상기할 수 있었다.
+* 포인터, 구조체와 같은 C언어 문법과 사용법을 배울 수 있었다.
+* 파일관련 함수(open, close), string관련 함수(strtok) 등 여러 함수들을 사용해 볼 수 있었다.
 
-* 
+#### Topological sorting
+* 선수과목, 난이도, 알파벳 순서의 기준으로 topological sorting을 진행하였다.
+* Topological sorting (위상 정렬)
+	* 어떤 일을 하는 순서를 찾는 알고리즘
+	* 방향 그래프에 존재하는 각 정점들의 선행 순서를 위배하지 않으면서 모든 정점을 나열하는 것
+<img height="300" src="https://image.slidesharecdn.com/lecture16-140420071804-phpapp02/95/topological-sort-8-638.jpg?cb=1397978457"></img>
+[리디렉션 알림](https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjn_fr944bhAhXYUd4KHWYaDVYQjRx6BAgBEAU&url=https%3A%2F%2Fwww.slideshare.net%2Fsandpoonia%2Flecture16-33728751&psig=AOvVaw3uribqjcyilSbckrlU7Bq-&ust=1552829811135290)
 
-## Feedback for better class
 
-* Please give us a harder one than this. We're not idiot dude.
+## 과제에 대한 피드백
+* C언어를 오랜만에 접하는 것이다 보니 문법적인 부분이나 사용에 있어서 어려움이 있었다. 그리고 이러한 점에서 많은 시행착오를 겪고, 문제해결을 위해 하나하나 찾아가면서 진행했다. C언어를 좀 더 공부해서 어려움을 줄여나가야 할 것 같다.
